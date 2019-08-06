5e0e6a9bc44d61fb1c3334e7d1d35090489aa8ef
dclemenzi <dclemenzi@incadencecorp.com>
2019-08-06 17:10:46 -0400

    Replaced deploy.sh with the maven pom file to do the copying.
    Updated poms to export the correct packages.
    
    Stubbed out a mattspresso blueprint

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-cp mattspresso-api/target/*.jar docker-compose/deploy
-cp mattspresso-model/target/*.jar docker-compose/deploy
-cp mattspresso-controller/target/*.jar docker-compose/deploy
-cp mattspresso-view/target/*.war docker-compose/deploy
