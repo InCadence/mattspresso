e33ca11ff1bb90b272f38e57498180d6553dde84
Michael Daconta <michael.daconta@gmail.com>
2019-08-09 12:56:24 -0400

    admin dashboard first cut

--- mattspresso-view/src/components/index.js
+++ mattspresso-view/src/components/index.js
@@ -2,3 +2,4 @@ export { default as UserList } from './UserList';
 export { default as CurrentUser } from './CurrentUser'
 export { default as MainDashboard } from './MainDashboard'
 export { default as OrderForm } from './OrderForm';
+export { default as AdminDashboard } from './AdminDashboard'
