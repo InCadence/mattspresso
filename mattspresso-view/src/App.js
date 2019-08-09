6151b358fc466bd2ec7f6b05179b877e5753ac74
dclemenzi <dclemenzi@incadencecorp.com>
2019-08-09 12:43:49 -0400

    Resolved Issue

--- mattspresso-view/src/App.js
+++ mattspresso-view/src/App.js
@@ -18,8 +18,6 @@ import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 import { clearError } from './actions';
 import { login, logout } from './actions/userActions';
